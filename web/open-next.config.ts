5642ce809dedb5de92105dbdfb8faf2c79b232df
matholamew <matthew+github@manderson.me>
2026-02-10 13:50:40 -0500

    fix: install opennextjs dependency and add config

--- web/open-next.config.ts
+++ web/open-next.config.ts
@@ -1,6 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-  // This is the default configuration for Next.js 15 on Cloudflare
-  // We can add R2 caching here later
+  // Leave empty for now to use defaults
 });
